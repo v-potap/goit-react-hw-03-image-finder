7e2c4a554cc42d8270b552d2c29a73b1a55ea011
v-potap <v.o.potapenko@gmail.com>
2019-09-07 16:53:56 +0300

    Updates

---
+++ precache-manifest.c27c7aa79572439df9127c0f08cc72e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a52b2a5202afac2d6fb53597547fcb",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "39dffc31647c51cff544",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "9708e69790a874ccf171",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.31969112.chunk.css"
+  },
+  {
+    "revision": "39dffc31647c51cff544",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.f57f7ed3.chunk.js"
+  },
+  {
+    "revision": "9708e69790a874ccf171",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.5088001e.chunk.js"
+  },
+  {
+    "revision": "c84682b687beae479976",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.ca18ead5.js"
+  }
+]);
